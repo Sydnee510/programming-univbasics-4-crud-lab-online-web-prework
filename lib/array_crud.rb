b8695f0317cbffcff892f9512bc2a655b7ec2143
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-01 08:30:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element) 
-   battery = ["wow", "I", "am", "really", "learning"]
-  battery.push("arrays!")
-  p battery
+  array = ["wow", "I", "am", "really", "learning"]
+   array.push("arrays!")
+   p array
 end
 
 def add_element_to_start_of_array(array, element)
