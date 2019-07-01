5c91dd92a595c4c5f7bf1bed87fbad3d7154dc1b
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-01 06:34:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ ["cheese","mayo","mustard","greens"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ ["wow","I","am","really","learning"]
+ element << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
